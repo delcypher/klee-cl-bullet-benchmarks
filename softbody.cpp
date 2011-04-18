53b22ffdc170ff38398f23d5a492cabf125661a4
Peter Collingbourne <pcc03@doc.ic.ac.uk>
2011-04-18 17:55:44 +0100

    Exercise more of the soft body solver

--- softbody.cpp
+++ softbody.cpp
@@ -1,12 +1,30 @@
 #include <BulletMultiThreaded/GpuSoftBodySolvers/OpenCL/btSoftBodySolver_OpenCL.h>
+#include <BulletSoftBody/btSoftBody.h>
 #include <clstuff.h>
+#include <klee/klee.h>
 
 extern cl_context                       g_cxMainContext;
 extern cl_device_id                     g_cdDevice;
 extern cl_command_queue         g_cqCommandQue;
 
+using namespace klee;
+
 int main(int argc, char **argv) {
   initCL(0, 0);
   btOpenCLSoftBodySolver cls(g_cqCommandQue, g_cxMainContext);
   cls.checkInitialized();
+//  cls.solveConstraints(1.0);
+  
+  btSoftBodyWorldInfo worldInfo;
+  worldInfo.air_density = symbolic<btScalar>("air_density");
+  worldInfo.water_density = symbolic<btScalar>("water_density");
+  worldInfo.water_offset = symbolic<btScalar>("water_offset");
+  worldInfo.water_normal = symbolic<btVector3>("water_normal");
+  worldInfo.m_gravity = symbolic<btVector3>("m_gravity");
+
+  btSoftBody body1(&worldInfo, 3, symbolic<btVector3[3]>("x"), symbolic<btScalar[3]>("m"));
+
+  btAlignedObjectArray<btSoftBody *> bodies;
+  bodies.push_back(&body1);
+  cls.optimize(bodies);
 }
